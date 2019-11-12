e115e91d186ec08085502513ad05eaf1054d86c5
tospure <to.boontarik@gmail.com>
2019-11-12 14:29:23 +0700

    Start Project

---
+++ wp-content/themes/mesmerize-child/js/script.js
@@ -0,0 +1,31 @@
+
+// [ Load Jquery ==================================================================== ]
+// var script = document.createElement("SCRIPT");
+// script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+// script.type = 'text/javascript';
+// script.onload = function() {
+//     var $ = window.jQuery;
+//     // Use $ here...
+//     console.log( '1' );
+//     $(document).ready(function () {
+//         alert( 1234 );
+//         console.log( '2' );
+//     });
+// };
+// document.getElementsByTagName("head")[0].appendChild(script);
+
+// [ Javascript | Jquery ============================================================ ]
+$(document).ready(function () {
+    var pathname = window.location.pathname;    // = /@work/TripperTime/
+    var url = window.location.href;             // = http://localhost/@work/TripperTime/
+    var origin = window.location.origin;        // = http://localhost
+
+    // if( == TripperTime ) {
+    //     doWork1();
+    // }
+});
+
+function doWork1() {
+    // do it..
+}
+
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1574050995;s:7:"authKey";s:64:"VitM)FjN1MQ-A!4+MzOQ]d!X4xzNlhLqj7^fo/O~}GqE[5K kB|?QH 7N-c8%:,f";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2086;}
