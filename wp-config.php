76ad2fd388d56632498e102261bed09cd7cb8bc3
tospure <to.boontarik@gmail.com>
2019-11-12 15:20:12 +0700

    update code = /wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'trippertime' );
+define( 'DB_NAME', 'u130997546_trippertime' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u130997546_alerter' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Tt999999999' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '31.220.110.88' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
